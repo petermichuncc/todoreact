0cd6be912be9e8fc07ccb651cb15bf2468f5a957
petermichuncc <plarrimo@uncc.edu>
2017-01-17 21:21:36 -0500

    first commmit

---
+++ imports/api/tasks.js
@@ -0,0 +1,31 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
+ 
+export const Tasks = new Mongo.Collection('tasks');
+ 
+Meteor.methods({
+  'tasks.insert'(text) {
+    check(text, String);
+ 
+    // Make sure the user is logged in before inserting a task
+    
+ 	console.log("This is the text " + text)
+    Tasks.insert({
+      text,
+      createdAt: new Date()
+
+    });
+  },
+  'tasks.remove'(taskId) {
+    check(taskId, String);
+ 
+    Tasks.remove(taskId);
+  },
+  'tasks.setChecked'(taskId, setChecked) {
+    check(taskId, String);
+    check(setChecked, Boolean);
+ 
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+});
